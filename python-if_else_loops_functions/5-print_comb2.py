173cb8748dc56146710e82df3dd6e6dcae8c4931
a-sowa <axelsowapro@gmail.com>
2024-01-23 18:19:48 +0000

    Updating to fullfill requirements

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 for number in range(100):
-    if number < 10:
-        print(f"0{number},", end=' ')
-    elif number < 99:
-        print(f"{number},", end=' ')
-    else:
-        print(number)
+    print('{:02d}'.format(number), end=', ' if number < 99 else '\n')
