aa020107555ea282d460120fbb00d3ebb7fd63c9
a-sowa <axelsowapro@gmail.com>
2024-01-25 11:37:09 +0000

    Trying another string formating approach

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(0, len(str)):
-        char = str[i]
+    newString = ""
+    for i in range(len(str)):
         if ord(str[i]) > 96 and ord(str[i]) < 123:
-            char = chr(ord(str[i]) - 32)
-        if i + 1 == len(str):
-            print('{}'.format(char))
+            newString += chr(ord(str[i]) - 32)
         else:
-            print('{}'.format(char), end='')
+            newString += str[i]
+    print('{}'.format(newString))
