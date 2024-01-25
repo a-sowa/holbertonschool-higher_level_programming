88e0951d4f6d918278b1bb8ebce72e281619464c
a-sowa <axelsowapro@gmail.com>
2024-01-25 10:31:54 +0000

    Task 8 - Prints a string in uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(0, len(str)):
+        char = str[i]
+        if ord(str[i]) > 96 and ord(str[i]) < 123:
+            char = chr(ord(str[i]) - 32)
+        if i + 1 == len(str):
+            print(char)
+        else:
+            print(char, end='')
