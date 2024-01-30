65e1b97e0042e00e333e3182f9d8f37422de3604
a-sowa <axelsowapro@gmail.com>
2024-01-30 13:23:40 +0000

    I guess i am fixing things for thiss f**kin checker

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     result = []
-    for i in my_list:
+    for i in range(len(my_list)):
         result.append(True) if my_list[i] % 2 == 0 else result.append(False)
     return result
