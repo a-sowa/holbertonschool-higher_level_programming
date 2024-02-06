d9e0e8ae1856e7b868ef9b7d9960d315aeb34ae4
a-sowa <axelsowapro@gmail.com>
2024-02-06 10:16:28 +0000

    Tassk 1 - Class that defines a square by size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Class that defines a square
+"""
+
+
+class Square:
+    """
+    Function that creates an instance of Square with defined size
+    """
+    def __init__(self, size):
+        self.__size = size
