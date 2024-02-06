7b6c6d0e91d85e39b26a07f1deed00f9aea0ec48
a-sowa <axelsowapro@gmail.com>
2024-02-06 15:27:29 +0000

    Task 0 - Empty class that defines a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    Empty class named Rectangle
+"""
+
+
+class Rectangle:
+    def __init__(self):
+        """ Function that creates an instance of Rectangle """
+        pass
