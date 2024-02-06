25eb64ae9b6ae883b0078de59a0acb15cbf26b32
a-sowa <axelsowapro@gmail.com>
2024-02-06 11:03:09 +0000

    Task 3 - Same as task 2 but add a function that returns square area

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Class that defines a square
+"""
+
+
+class Square:
+    """
+    Function that creates an instance of Square with defined size
+    using type and value verification
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    """
+    Function that returns square's area
+    """
+    def area(self):
+        return self.__size * self.__size
