88a6c70bfe948ffe758e5226d1a11ad6726517c1
a-sowa <axelsowapro@gmail.com>
2024-02-08 16:53:52 +0000

    Task 0 - Module that adds two integers

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Module that adds two integers
+"""
+
+def add_integer(a, b=98):
+    """ Adds 2 integers and returns an integer: the addition of a and b """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
