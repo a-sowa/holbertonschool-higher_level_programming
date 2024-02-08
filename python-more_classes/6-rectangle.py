bb37723f7da9d5a9710acb3634d46e3370fb27bc
a-sowa <axelsowapro@gmail.com>
2024-02-08 12:23:27 +0000

    Task 6 - Same as task 5 but add a instances counter

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+"""
+    Class that defines a rectangle, its width, height,
+    area and perimeter and prints it
+"""
+
+
+class Rectangle:
+    """
+        Class that defines a rectangle, its width, height, area and perimeter
+    """
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """ Function that creates an instance of Rectangle """
+        self.height = height
+        self.width = width
+        Rectangle.number_of_instances += 1
+
+    @property
+    def height(self):
+        """ Getter function that retrieves the rectangle's height """
+        return self.__height
+
+    @property
+    def width(self):
+        """ Getter function that retrieves the rectangle's width """
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        """
+            Setter function that set and checks type and value of given height
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @width.setter
+    def width(self, value):
+        """
+            Setter function that set and checks type and value of given width
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        """ Function that returns rectangle's area """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """ Function that returns rectangle's perimeter """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return self.__width * 2 + self.__height * 2
+
+    def __str__(self):
+        """ Function that returns a printable representation of rectangle
+            with character '#' based on its height and width
+        """
+        string_to_print = ""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            for i in range(self.__height):
+                for j in range(self.__width):
+                    string_to_print += '#'
+                if i != self.__height - 1:
+                    string_to_print += '\n'
+            return string_to_print
+
+    def __repr__(self):
+        """  Function that returns a string representation of rectangle """
+        string_repr = "Rectangle(" + str(self.__width)
+        string_repr += ", " + str(self.__height) + ")"
+        return string_repr
+
+    def __del__(self):
+        """ Prints a message when deleting an instance and
+            decrements instances counter
+        """
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
