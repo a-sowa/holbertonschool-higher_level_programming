bc622a2f2ebe56d2f1d8c7baff150a35bd52781b
a-sowa <axelsowapro@gmail.com>
2024-02-08 09:29:14 +0000

    Fixing requirements

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,7 +7,7 @@
 class Square:
     def __init__(self, size=0):
         """ Function that creates an instance of Square with defined size """
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
