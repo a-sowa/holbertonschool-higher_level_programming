f30f0b284d99f9e09d698b68c4a8f542144fe9d3
a-sowa <axelsowapro@gmail.com>
2024-02-08 17:04:57 +0000

    Correcting exception message

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,10 +7,10 @@
 def add_integer(a, b=98):
     """ Adds 2 integers and returns an integer: the addition of a and b """
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
+        raise TypeError("a must be an integer")
 
     if not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
+        raise TypeError("b must be an integer")
 
     a = int(a)
     b = int(b)
