eedb6fe62b1f074e677525d33917b23fb3738351
a-sowa <axelsowapro@gmail.com>
2024-02-13 17:39:16 +0000

    Task 6 - Class with function that raises an exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    class named BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """class named BaseGeometry """
+    def area(self):
+        """ raises an Exception with the message area() is not implemented """
+        raise Exception("area() is not implemented")
