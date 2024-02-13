b52539dcd4f9057d9faeab06ac12cd69502be318
a-sowa <axelsowapro@gmail.com>
2024-02-13 16:21:56 +0000

    Adding class description

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,6 +5,9 @@
 
 
 class MyList(list):
+    """
+        A custom list class that inherits from the built-in list class.
+    """
     def print_sorted(self):
         """ Prints the elements of the list in sorted order (ascending). """
         sorted_list = sorted(self)
