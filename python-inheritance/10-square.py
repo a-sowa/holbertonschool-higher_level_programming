ac7ae1b6a2d53d05be130a346b480143a2edc54f
a-sowa <axelsowapro@gmail.com>
2024-02-13 20:06:56 +0000

    Fixing Pycodestyle

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,11 +4,11 @@
 """
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """Class representing a square, inheriting from Rectangle."""
     def __init__(self, size):
         """Initialize a square with a given size."""
-        super().__init__(size, size)
         self.__size = size
         self.integer_validator("size", size)
-
+        super().__init__(size, size)
