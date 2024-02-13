87bb642c4c74ae04ebe81a4e6a7ede17d8efdf83
a-sowa <axelsowapro@gmail.com>
2024-02-13 18:17:49 +0000

    Task 8 - Class that defines a rectangle, its width and height

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Class that defines a rectangle, its width and height.
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """ Class that defines a rectangle, its width and height. """
+    def __init__(self, width, height):
+        """
+            Creates an instance of Rectangle and checks for width
+            and height type and value
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
