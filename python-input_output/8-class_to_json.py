6f91bb761c33e5f364e80bb1ad08f5cea7390175
a-sowa <axelsowapro@gmail.com>
2024-02-14 10:22:34 +0000

    Task 8 - Func that returns the dictionary desc for JSON serialization of an object

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Function that returns the dictionary description with
+    simple data structure for JSON serialization of an object
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    Function that returns the dictionary description with
+    simple data structure for JSON serialization of an object
+    """
+    return json.dumps(obj.__dict__)
