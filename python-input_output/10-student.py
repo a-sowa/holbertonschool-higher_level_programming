b018c2566606f51879abc121f179a3fb399c4bf8
a-sowa <axelsowapro@gmail.com>
2024-02-14 11:07:29 +0000

    Changing return statement in to_json function

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -38,6 +38,5 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            for key, value in self.__dict__.items():
-                if key in attrs:
-                    return {key: value}
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
