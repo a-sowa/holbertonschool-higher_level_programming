59b836b7b76cb3ebd253bdb28bb39c1561f9fb72
a-sowa <axelsowapro@gmail.com>
2024-02-14 11:03:13 +0000

    Task 10 -  Same as task 9 but returns only selected attributes of student if exists

---
+++ python-input_output/10-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+A simple class representing a Student and a method
+to convert its attributes to a JSON-like dictionary.
+"""
+
+
+class Student:
+    """
+    Represents a Student with first name, last name, and age attributes.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new instance of the Student class.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Args:
+            attrs (list, optional): A list of strings specifying attribute
+                                    names to be retrieved.
+                                    If None, retrieve all attributes.
+
+        Returns:
+            dict: A dictionary representing the selected attributes of the student.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    return {key: value}
