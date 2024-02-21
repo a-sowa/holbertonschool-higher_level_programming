105e72580baf9536f18195446847f8399df0f330
a-sowa <axelsowapro@gmail.com>
2024-02-21 11:06:05 +0000

    Task 8 - Update the Rectangle attributes using the provided arguments in order

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -114,3 +114,15 @@ class Rectangle(Base):
         str_1 = f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"
         str_2 = f" - {self.__width}/{self.__height}"
         return f"{str_1}{str_2}"
+
+    def update(self, *args):
+        """
+            Update the Rectangle attributes using the
+            provided arguments in order.
+        """
+        if len(args) > 0:
+            self.id = args[0] if len(args) >= 1 else self.id
+            self.width = args[1] if len(args) >= 2 else self.width
+            self.height = args[2] if len(args) >= 3 else self.height
+            self.x = args[3] if len(args) >= 4 else self.x
+            self.y = args[4] if len(args) >= 5 else self.y
