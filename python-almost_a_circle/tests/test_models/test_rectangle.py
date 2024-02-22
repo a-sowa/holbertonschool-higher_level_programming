9e2a979a9cecf6e0f69d1ec5e8860bbdca74c474
a-sowa <axelsowapro@gmail.com>
2024-02-22 18:55:07 +0000

    Updating tests file

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -50,6 +50,92 @@ class testRectangleCreation(unittest.TestCase):
         with self.assertRaisesRegex(ValueError, "y must be >= 0"):
             rectangle1 = Rectangle(23, 12, 4, -1)
 
+    def test_rectangle_subclass(self):
+        """
+        Test if Rectangle class inherit from
+        Base class
+        """
+        self.assertTrue(issubclass(Rectangle, Base))
+    def test_parameters(self):
+        """
+        Test parameters for Rectangle class
+        """
+        r1 = Rectangle(10, 2)
+        r2 = Rectangle(2, 10)
+        r3 = Rectangle(10, 2, 0, 0, 12)
+
+        self.assertEqual(r1.width, 10)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r1.y, 0)
+        self.assertEqual(r2.width, 2)
+        self.assertEqual(r2.height, 10)
+        self.assertEqual(r2.x, 0)
+        self.assertEqual(r2.y, 0)
+        self.assertEqual(r3.id, 12)
+        self.assertEqual(r3.width, 10)
+        self.assertEqual(r3.height, 2)
+        self.assertEqual(r3.x, 0)
+        self.assertEqual(r3.y, 0)
+        with self.assertRaises(TypeError):
+            r4 = Rectangle()
+    def test_string(self):
+        """
+        Test string parameters for a
+        Rectangle class
+        """
+        with self.assertRaises(TypeError):
+            Rectangle('Monty', 'Python')
+    def test_type_param(self):
+        """
+        Test different types of parameters
+        for a Rectangle class
+        """
+        with self.assertRaises(TypeError):
+            Rectangle(1.01, 3)
+            raise TypeError()
+        with self.assertRaises(ValueError):
+            Rectangle(-234234242, 45)
+            raise ValueError()
+        with self.assertRaises(TypeError):
+            Rectangle('', 4)
+            raise TypeError()
+        with self.assertRaises(TypeError):
+            Rectangle(True, 4)
+            raise TypeError()
+        with self.assertRaises(TypeError):
+            Rectangle(5, 1.76)
+            raise TypeError()
+        with self.assertRaises(TypeError):
+            Rectangle(5, "Hello")
+            raise TypeError()
+        with self.assertRaises(ValueError):
+            Rectangle(5, -4798576398576)
+            raise ValueError
+        with self.assertRaises(TypeError):
+            Rectangle(5, 1, 1.50)
+            raise TypeError()
+        with self.assertRaises(TypeError):
+            Rectangle(5, 6, "test")
+            raise TypeError()
+        with self.assertRaises(TypeError):
+            Rectangle(5, 7, False)
+            raise TypeError()
+        with self.assertRaises(ValueError):
+            Rectangle(5, 7, -4798576398576)
+            raise ValueError()
+        with self.assertRaises(TypeError):
+            Rectangle(5, 1, 1, 1.53)
+            raise TypeError()
+        with self.assertRaises(TypeError):
+            Rectangle(5, 6, 5, "test")
+            raise TypeError()
+        with self.assertRaises(TypeError):
+            Rectangle(5, 7, 7, False)
+            raise TypeError()
+        with self.assertRaises(ValueError):
+            Rectangle(5, 9, 5, -4798576398576)
+            raise ValueError()
 
 class TestRectangleIdIdentation(unittest.TestCase):
     """Tests for check the id attribution of all instance"""
