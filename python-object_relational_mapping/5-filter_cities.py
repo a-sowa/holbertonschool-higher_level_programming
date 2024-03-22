fbebf0ff29e20f2cc10fde107375b05f985b9eb7
a-sowa <axelsowapro@gmail.com>
2024-03-22 10:17:42 +0000

    Fix

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-    Script that takes in the name of a state as an argument and
-    lists all cities of that state, using the database hbtn_0e_4_usa
+    Script that takes in the name of a state as an argument and lists
+    all cities of that state, using the database hbtn_0e_4_usa
 """
 import MySQLdb
 import sys
@@ -11,17 +11,23 @@ def filter_cities_by_state(username, password, database, state_name):
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=database)
     cursor = db.cursor()
-    sql_query = "SELECT GROUP_CONCAT(name SEPARATOR ', ') FROM cities \
-        JOIN states ON cities.state_id = states.id WHERE states.name = %s"
 
-    cursor.execute(sql_query, (state_name,))
+    cursor.execute("SELECT cities.id, cities.name, states.name \
+                FROM cities INNER JOIN states \
+                WHERE cities.state_id = states.id \
+                AND states.name= '{}' \
+                ORDER BY id".format(state_name))
     cities = cursor.fetchall()
 
-    if cities:
-        for city in cities:
-            print(city)
-    else:
-        print("")
+    for city in cities:
+        print(city[1], end='')
+        if cities.index(city) != len(cities) - 1:
+            print(", ", end="")
+        else:
+            print()
+    if not len(cities):
+        print()
+
 
     cursor.close()
     db.close()
