8ffffa62bd2b6bdaba4f8825afa8517d345e0de7
a-sowa <axelsowapro@gmail.com>
2024-05-22 05:52:40 +0000

    Task 1 - script that updates the text color of the header element to red (#FF0000) when the user clicks on the tag with id red_header

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,3 @@
+const red_header = document.querySelector('#red_header');
+const header = document.querySelector('header');
+red_header.addEventListener('click', function () { header.style.color = '#FF0000'; });
